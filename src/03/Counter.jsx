dd4a512d05230601d2e4cf2bd000b7f385722549
joshua <iuttn123@gmail.com>
2021-11-22 16:30:16 +0900

    state

--- src/03/Counter.jsx
+++ src/03/Counter.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import StateExample from "./StateExample";
 
 class Counter extends React.Component{
-    constructor(props) {
-        super(props);
+    constructor(props, context) {
+        super(props, context);
         this.state={
-            count:5,
+            count: 1,
         };
+        this.increaseCount = this.increaseCount.bind(this);
     }
 
     increaseCount(){
-        this.state.count = 6;
-
+        this.setState(({count}) => ({
+            count : count+1,
+        }));
     }
 
     render() {
         return(
             <div>
                 <span>카운트: {this.state.count}</span>
-                <button onClick={this.increaseCount()}> 카운트 증가!</button>
+                <button onClick={this.increaseCount}> 카운트 증가!</button>
             </div>
         )
     }
